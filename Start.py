f94186d5080d2f1c4b4feb0e2d2251d99d8ff472
frozzale <frozzale@students.zhaw.ch>
2025-04-09 10:32:34 +0200

    Save

--- Start.py
+++ Start.py
@@ -6,7 +6,7 @@ from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
 # initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="AppV.1")  # switch drive 
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="AppV1")  # switch drive 
 
 # initialize the login manager
 login_manager = LoginManager(data_manager)
