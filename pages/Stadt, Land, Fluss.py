91b6d13476fe014846bc175fdc53b8565fcd8b48
frozzale <frozzale@students.zhaw.ch>
2025-05-07 12:42:47 +0200

    Themenergänzung

--- pages/Stadt, Land, Fluss.py
+++ pages/Stadt, Land, Fluss.py
@@ -31,9 +31,12 @@ kategorien = {
     "Marken": ["Marke", "Auto", "Mode", "Technologie", "Lebensmittel", "Kosmetik"],
     "Sport": ["Sportart", "Olympische Disziplin", "Extremsportarten", "Sportart ohne Ball"],
     "Musik": ["Instrument", "Band", "Sänger", "Genre", "Lied"],
-    "Filme & Serien": ["Film", "Serie", "Schauspieler", "Regisseur", "Genre"],
+    "Filme & Serien": ["Film", "Serie", "Schauspieler", "Regisseur", "Genre", "Buchtitel"],
     "Essen & Trinken": ["Getränk", "Süßigkeit", "Hauptgericht", "Beilage", "Snack", "Pizzabelag", "Eissorte"],
-    "Prominenz": ["Schauspieler", "Sänger", "Fussballspieler", "Politiker", "Historische Persönlichkeit", "Film-/Serie-/Buch-Charakter"]
+    "Prominenz": ["Schauspieler", "Sänger", "Fussballspieler", "Politiker", "Historische Persönlichkeit", "Film-/Serie-/Buch-Charakter"],
+    "Lustig & ungewöhnlich": ["Scheidungsgrund / Kündigungsgrund", "Jugendwort", "Gegenstände, die die Welt nicht braucht", "Kosenahme", "Superkraft", "Dinge im Haushalt", "Haustiernamen"],
+    "Themen zum Kennenlernen": ["Würde ich nie essen", "Reiseziel", "Würde ich kaufen, wenn ich reich wäre", "Hab ich noch nie gemacht", "Mag ich gerne"],
+    "Babyparty": ["Babynamen", "Könnte das erste Wort werden", "Geschmacksrichtung für Babybrei", "Hätte ich gerne als Kind gehabt"]
 } 
 
 # Streamlit App
